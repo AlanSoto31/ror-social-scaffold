9fb587be1b6a433222cee1688ba50dec657b64ca
janis-jenny <pcardenasvm@gmail.com>
2021-01-20 17:05:12 -0500

    add avatar for users, edit profile link in the index's user & add destroy in the user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
-  has_many :friendships
-
-  has_many :accepted_friendships, -> { where(status: true) }, class_name: 'Friendship'
-  has_many :pending_friendships, -> { where(status: false) }, class_name: 'Friendship'
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friendships, dependent: :destroy
+  has_many :accepted_friendships, -> { where(status: true) }, class_name: 'Friendship', dependent: :destroy
+  has_many :pending_friendships, -> { where(status: false) }, class_name: 'Friendship', dependent: :destroy
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
 
   def friends
     Friendship.where(user_id: id, status: true).pluck(:friend_id)
@@ -23,5 +21,11 @@ class User < ApplicationRecord
   def list_friends
     ids = Friendship.where(user_id: id, status: true).pluck(:friend_id)
     User.where(id: ids)
+  end
+
+  def avatar
+    email_address = email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    "https://www.gravatar.com/avatar/#{hash}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_001951) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "friendships", "users"
-  add_foreign_key "friendships", "users", column: "friend_id"
+  add_foreign_key "friendships", "users", on_delete: :cascade
+  add_foreign_key "friendships", "users", column: "friend_id", on_delete: :cascade
+
 end
